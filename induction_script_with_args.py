f1b205089d3483b97e3e3e19a7f9e30f929bfec4
Genevieve Johnson <76914820+genevieve-johnson@users.noreply.github.com>
2022-03-28 14:41:22 -0500

    Update induction_script_with_args.py

--- induction_script_with_args.py
+++ induction_script_with_args.py
@@ -7,7 +7,7 @@ import subprocess
 
 
 def msg(name=None):
-    return '''induction_script_with_args.py -p phage_reference_file [read file options] -R Rscript_path -r R_code_path -s path_to_software_tools -o output_path_and_sample_name'''
+    return '''induction_script_with_args.py -f phage_reference_file [read file options] -R Rscript_path -r R_code_path -s path_to_software_tools -o output_path_and_sample_name'''
 
 
 parser=argparse.ArgumentParser(usage=msg())
